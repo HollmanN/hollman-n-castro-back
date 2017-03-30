149be8ddbf587df0280fbee5d3a10433606137f0
Hollman Castro <hollman.castro@cloudrtanalytics.com>
2017-03-30 17:01:20 -0400

    Added Point3D class

---
+++ src/main/java/Point3D.java
@@ -0,0 +1,42 @@
+/**
+ * @author Hollman N
+ *
+ * Created on 3/30/17.
+ */
+
+public class Point3D {
+    private int x;
+    private int y;
+    private int z;
+
+    public Point3D(int x, int y, int z) {
+        super();
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public int getZ() {
+        return z;
+    }
+
+    public void setZ(int z) {
+        this.z = z;
+    }
+}
